412fee3fa691037c91b068d77063be3c8fcf8ad8
yilinfeng <798555920@qq.com>
2015-11-04 19:06:26 +0800

    添加必要文件

---
+++ src/main/java/com/earl/fishshop/util/MyConstant.java
@@ -0,0 +1,19 @@
+package com.earl.fishshop.util;
+/**
+ * @author 黄祥谦.
+ * @date:2015-11-4 下午4:34:38
+ * @version :
+ */
+public class MyConstant {
+
+	/**
+	 * 审查通过.
+	 */
+	public static final Integer checkout_pass = 1;
+	
+	/**
+	 * 用户认证通过
+	 */
+	public static final Integer user_pass = 1;
+
+}
