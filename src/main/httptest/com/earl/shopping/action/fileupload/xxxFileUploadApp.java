2c9447497db327865748f2a5f654c997deef355d
yilinfeng <798555920@qq.com>
2015-11-23 22:02:36 +0800

    x

---
+++ src/main/httptest/com/earl/shopping/action/fileupload/xxxFileUploadApp.java
@@ -0,0 +1,67 @@
+package com.earl.shopping.action.fileupload;
+
+import java.io.File;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+/**
+ * @author 黄祥谦.
+ * @date:2015-11-23 下午9:45:48
+ * @version :
+ */
+public class xxxFileUploadApp {
+	private File targetFile;
+
+	public void dod() {
+		String targetURL = "http://localhost:8080/fishshop/category_addCategory.action";
+		targetFile = new File(
+				"D:/Favorites/test/test.jpg");
+
+		PostMethod filePost = new PostMethod(targetURL);
+
+		// filePost.getParams().setBooleanParameter(HttpMethodParams.USE_EXPECT_CONTINUE,
+		// cbxExpectHeader.isSelected());
+		try {
+			
+			Part[] parts = { 
+			 new FilePart("categoryFile.file", targetFile)
+			,new FilePart("categoryFile.file", new File("C:/Users/Administrator/Desktop/struts2流程图.png"))
+			,new StringPart("categorySimpleName", "东星白班鱼","utf-8")
+			,new StringPart("categoryAcademicName", "东星白班鱼","utf-8")
+			,new StringPart("getType", "3","UTF-8")
+			,new StringPart("categoryEnglishName", "东星白班鱼","utf-8")
+			,new StringPart("parentId", "","utf-8")
+			,new StringPart("unit", "1","utf-8")
+			,new StringPart("totalSellNumber", "9","utf-8")
+			// new FilePart(targetFile.getName(), targetFile)
+			};
+			HttpMethodParams params = filePost.getParams();
+//			String contentCharset = params.getContentCharset();
+			params.setContentCharset("utf-8");
+			filePost.setRequestEntity(new MultipartRequestEntity(parts,
+					filePost.getParams()));
+			HttpClient client = new HttpClient();
+			client.getHttpConnectionManager().getParams()
+					.setConnectionTimeout(50000);
+			int status = client.executeMethod(filePost);
+			if (status == HttpStatus.SC_OK) {
+			} else {
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			filePost.releaseConnection();
+		}
+	}
+
+	public static void main(String[] args) {
+		new CategoryFileUploadApp().dod();
+	}
+}
---
+++ src/main/java/com/earl/fishshop/domain/category/CategoryForNextLevel.java
@@ -0,0 +1,24 @@
+package com.earl.fishshop.domain.category;
+
+
+@SuppressWarnings("unused")
+public class CategoryForNextLevel{
+
+	private Long categoryId ;
+	
+	private String categorySimpleName ;
+	
+	private String categoryAcademicName ;
+	
+	private String categoryEnglishName ;
+	
+	private String fishPhoto ;
+	
+	private Double lowPrice ;//
+	
+	private Long hotDegree;
+	
+	private String unit ;
+	
+	private Long totalSellNumber ;
+}
