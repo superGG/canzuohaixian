712f3c1fbc789d0f27de051f4fbd6df7a0f13124
yilinfeng <798555920@qq.com>
2015-11-25 22:06:09 +0800

    x

---
+++ src/main/java/com/earl/fishshop/vo/MulitFileVo.java
@@ -0,0 +1,39 @@
+package com.earl.fishshop.vo;
+
+import java.io.File;
+import java.util.List;
+
+
+public class MulitFileVo{
+	private List<File> file;
+	
+	private List<String> contentType;
+
+	private List<String> fileName;
+
+	public List<File> getFile() {
+		return file;
+	}
+
+	public void setFile(List<File> file) {
+		this.file = file;
+	}
+
+	public List<String> getFileContentType() {
+		return contentType;
+	}
+
+	public void setFileContentType(List<String> contentType) {
+		this.contentType = contentType;
+	}
+
+	public List<String> getFileFileName() {
+		return fileName;
+	}
+
+	public void setFileFileName(List<String> fileName) {
+		this.fileName = fileName;
+	}
+
+
+}
