efe51085b14ef5e5e6d2296b2fa72fea2adb44e6
superGG <459714770@qq.com>
2015-11-27 16:35:27 +0800

    添加http测试

---
+++ src/main/httptest/com/earl/shopping/action/HttpCommentActionTest.java
@@ -0,0 +1,32 @@
+package com.earl.shopping.action;
+
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.junit.Test;
+
+public class HttpCommentActionTest extends BaseActionTest{
+
+	@Test
+	public void testAddComment(){
+//		String targetURL = "http://192.168.1.107:8080/fishshop/category_getNextLevelCategory.action";
+//		String targetURL = "http://www.earltech.cn:8080/fishshop/category_getNextLevelCategory.action";
+		String targetURL = "http://localhost:8080/fishshop/comment_addComment.action";
+		PostMethod filePost = new PostMethod(targetURL);
+		Part[] parts = { 
+				new StringPart("userId", "1","utf-8")
+				,new StringPart("shopId", "1","utf-8")
+				,new StringPart("ordersId", "11111110","UTF-8")
+				,new StringPart("commentType", "-1","utf-8")
+				,new StringPart("content", "不错。我只是来测试而已","utf-8")
+				,new StringPart("weightQuality", "3","UTF-8")
+				,new StringPart("freshQuality", "2","utf-8")
+				,new StringPart("speedQuality", "4","utf-8")
+				,new StringPart("anonymity", "1","UTF-8")
+		};
+		String sendHttpRequest = sendHttpRequest(filePost, parts);
+		System.out.println(sendHttpRequest);
+		
+	}
+      
+}
--- src/main/test/com/earl/shopping/action/CommentActionTest.java
+++ src/main/test/com/earl/shopping/action/CommentActionTest.java
@@ -42,15 +42,14 @@ public class CommentActionTest extends StrutsSpringTestCase {
 	public void testAddComment() throws Exception {
 		// request.addHeader("Content-type", " text/html; charset=utf-8");
 //
-		request.addParameter("userId", "1");
-		request.addParameter("shopId", "1");
+		request.addParameter("userId", "5");
+		request.addParameter("shopId", "5");
 		request.addParameter("ordersId", "11111120");
 		request.addParameter("commentType", "-1");
 		request.addParameter("content", "不错。我只是来测试而已");
 		request.addParameter("weightQuality", "3");
 		request.addParameter("freshQuality", "2");
 		request.addParameter("speedQuality", "4");
-//		request.addParameter("createTime", "2015-11-17 11:17:58");
 		request.addParameter("anonymity", "1");
 
 		String res = executeAction("/comment_addComment.action");
