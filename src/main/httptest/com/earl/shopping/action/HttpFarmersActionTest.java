8fef08a53671ae7035cacd8bb81a90e027363b7b
yilinfeng <798555920@qq.com>
2015-11-30 20:49:15 +0800

    x

---
+++ src/main/httptest/com/earl/shopping/action/HttpFarmersActionTest.java
@@ -0,0 +1,26 @@
+package com.earl.shopping.action;
+
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.junit.Test;
+
+public class HttpFarmersActionTest extends BaseActionTest {
+
+	private String bastPath = "http://192.168.1.107:8080";
+//	 private String bastPath = "http://www.earltech.cn:8080";
+
+	@Test
+	public void testGetFarmerByShop() {
+		String targetURL = bastPath
+				+ "/fishshop/farmers_getFarmerByShop.action";
+
+		PostMethod filePost = new PostMethod(targetURL);
+
+		Part[] parts = { new StringPart("shopId", "5", "utf-8"),
+		};
+		String sendHttpRequest = sendHttpRequest(filePost, parts);
+		System.out.println(sendHttpRequest);
+	}
+
+}
